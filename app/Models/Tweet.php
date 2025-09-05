194f74f87e2ce333a9b07ea0accf88e3e234dbbd
gachi-git <gachikogay@gmail.com>
2025-09-05 19:17:41 +0900

    ルーティング完了

--- app/Models/Tweet.php
+++ app/Models/Tweet.php
@@ -9,4 +9,14 @@ class Tweet extends Model
 {
     /** @use HasFactory<\Database\Factories\TweetFactory> */
     use HasFactory;
+
+// fillableはモデルに対して一括代入を許可する属性のリストを指定するためのプロパティです。
+// これにより、セキュリティリスクを軽減し、意図しない属性の変更を防ぐことができます。
+    protected $fillable = ['tweet'];
+
+    //一対多で自分が「多」の側なのでbelongsToを使う
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
